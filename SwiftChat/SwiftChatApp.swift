d1e5a03b67d83df340b19a5f776ef9c7389e94d3
Rajaram Mohanty <rajaram.mohanty@wedopulse.co.in>
2023-05-08 16:03:44 +0530

    Init project

---
+++ SwiftChat/SwiftChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChatApp.swift
+//  SwiftChat
+//
+//  Created by Rajaram on 08/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
